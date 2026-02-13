cc5107314f1f3a925273f64b34c2bf98d5f5e2ab
Cabdulahi Hussein <abdulahih0614@gmail.com>
2026-02-13 13:18:20 -0800

    Remove CISC 111 and CISC 211 folders

--- CISC 111/Homework/tempretature.cpp
+++
@@ -1,8 +0,0 @@
-/******************************************************************************
-Cabdulahi Hussein
-CS111
-Homework 2 - temperature.cpp
-02/10/2025
-
-This program will ask for a Celsius temperature and convert it to Fahrenheit temperature and round it to the nearest integer.
-******************************************************************************/
--- CISC 211/Demos/OOP-Demo/4-InterfaceClass/client.cpp
+++
@@ -1,9 +0,0 @@
-// CS311 Yoshii - testing the girl class that inherited from Ipure 
-// -----------------------------------------------------------
-#include"girl.h"
-
-int main()
-{
-  girl Mary;
-  Mary.doit();
-}
